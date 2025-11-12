d1377db524fcaa92802c3270077557dbe9e4c77b
Malars_Icaru <malarsicaru9@gmail.com>
2025-11-12 09:56:18 +0100

    TP_1 Terminé

--- TP_1.c
+++ TP_1.c
@@ -132,9 +132,126 @@ void exo_13(int n)
         n--;
     }
 }
-void exo_14()
+void exo_14(int n)
 {
-    int total =
+    int somme = 0;
+    while (n != 1)
+    {
+        somme += n;
+        n --;
+    }
+}
+int exo_14_version_recursive(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    return n + exo_14_version_recursive(n-1);
+}
+int exo_15(int n)
+{
+    int compteur = 0;
+    while (n > 1)
+    {
+        n /= 10;
+        compteur ++;
+        if (n<1)
+        {
+            return compteur;
+        }
+    }
+    return 1;
+}
+void exo_16(int  n)
+{
+    int renverser = 0;
+    while (n > 0)
+    {
+        renverser = n * 10 + (n % 10);
+        n /= 10;
+    }
+}
+void exo_17(int n)
+{
+    if (n <= 1) {
+        printf("%d is not a prime number\n", n);
+        return;
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            printf("%d is not a prime number\n", n);
+            return;
+        }
+    }
+
+    printf("%d is a prime number\n", n);
+}
+
+int request_number ()
+{
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    return number;
+}
+void exo_18()
+{
+    int a = request_number();
+    int b = request_number();
+
+    if (a < 0 || b < 0)
+    {
+        printf("The number need to be more than 0");
+    }
+    else
+    {
+        while (b > 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+            printf("%i %i ", a, b);
+        }
+    }
+}
+
+void exo_19()
+{
+    int medium = 0;
+    int tab[] = {1,2,3,6,4,8,4,1};
+    int max = tab[0];
+    int min = tab[0];
+    printf("%d ",min);
+    for (int i = 0; i < 8; i++)
+    {
+        if (tab[i] > max)
+        {
+            max = tab[i];
+        }
+        if (tab[i] < min)
+        {
+            min = tab[i];
+        }
+        medium+= tab[i];
+    }
+    medium /= 8;
+    printf("%i %i %i\n",max,min,medium);
+}
+void exo_20()
+{
+    char mot[10] = "salutlele";
+    int compteur = -1;
+
+    for (int i = 0; i <= 10; i++)
+    {
+        if (mot[i] != '\0')
+        {
+            compteur++;
+        }
+    }
+    printf("%i",compteur);
 }
 int main(void)
 {
@@ -143,8 +260,11 @@ int main(void)
     // exo_3(1,2);
     // exo_4(10,20,30);
     // exo_5(18);
-    exo_6(10,20);
-    exo_12(9);
+    //exo_6(10,20);
+    //exo_12(9);
+    //exo_18();
+    exo_19();
+    exo_20();
 
     return 0;
 }
